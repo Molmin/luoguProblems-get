38628e39b1799cca1f43df4c1755f35dfc10dd14
Molmin <115549630@qq.com>
2023-06-14 16:28:36 +0800

    core: optimize (use json)

--- getlist.js
+++ getlist.js
@@ -1,24 +1,25 @@
-const {writeFileSync}=require('fs');
-var https=require('https'); 
+const { writeFileSync } = require('fs');
+var https = require('https');
 
-var cntPages=1,type=process.argv[2];
+var cntPages = 1, type = process.argv[2];
 
-var problemList={};
+var problemList = {};
 
-var getList=(pageId)=>{
-    if(pageId>cntPages)return;
+var getList = (pageId) => {
+    if (pageId > cntPages) return;
     console.log(`Getting Page #${pageId} (${pageId}/${cntPages})`);
-    https.get(`https://www.luogu.com.cn/problem/list?_contentOnly=1&type=${type}&page=${pageId}`,(res)=>{
-        let data='';
-        res.on('data',chunk=>data+=chunk);
-        res.on('end',()=>{
-            var list=JSON.parse(data).currentData.problems;
-            if(pageId==1)cntPages=Math.ceil(list.count/list.perPage);
-            list.result.forEach(problem=>problemList[problem.pid]=problem);
-            writeFileSync(`data/${type}.json`,JSON.stringify(problemList));
+    https.get(`https://www.luogu.com.cn/problem/list?_contentOnly=1&type=${type}&page=${pageId}`, (res) => {
+        let data = '';
+        res.on('data', chunk => data += chunk);
+        res.on('end', () => {
+            var list = JSON.parse(data).currentData.problems;
+            if (pageId == 1) cntPages = Math.ceil(list.count / list.perPage);
+            list.result.forEach(problem => problemList[problem.pid]
+                = { pid: problem.pid, type: problem.type, title: problem.title });
+            writeFileSync(`data/${type}.json`, JSON.stringify(problemList));
         });
     });
-    setTimeout(()=>getList(pageId+1),pageId==1?2000:800);
+    setTimeout(() => getList(pageId + 1), pageId == 1 ? 2000 : 800);
 }
 
 getList(1);
--- getproblems.js
+++ getproblems.js
@@ -1,36 +1,62 @@
-const {writeFileSync,readFileSync,mkdirSync,existsSync}=require('fs');
-var https=require('https');
+const { writeFileSync, readFileSync, mkdirSync, existsSync } = require('fs');
+var https = require('https');
 
-var type=process.argv[2];
+var type = process.argv[2];
 
-if(!existsSync(`data/${type}`))mkdirSync(`data/${type}`);
-var problemList=JSON.parse(readFileSync(`data/${type}.json`,'utf8'));
-var problemIdList=new Array();
-for(var key in problemList)
+if (!existsSync(`data/${type}`)) mkdirSync(`data/${type}`);
+var problemList = JSON.parse(readFileSync(`data/${type}.json`, 'utf8'));
+var problemIdList = new Array();
+for (var key in problemList)
     problemIdList.push(key);
+var current, currentJsonId;
+var getCurrentData = (type, jsonId) => {
+    if (existsSync(`data/${type}/${jsonId}.json`))
+        return JSON.parse(readFileSync(`data/${type}/${jsonId}.json`));
+    else return {};
+}
+current = getCurrentData(type, currentJsonId = 1);
 
-var getProblem=(id)=>{
-    if(id==problemIdList.length)return;
-    var PID=problemIdList[id];
-    if(existsSync(`data/${type}/${PID}`)){
-        getProblem(id+1);
-    }
-    else{
-        console.log(`Getting Problem ${PID} (${id+1}/${problemIdList.length})`);
-        https.get(`https://www.luogu.com.cn/problem/${PID}?_contentOnly=1`,(res)=>{
-            let data='';
-            res.on('data',chunk=>data+=chunk);
-            res.on('end',()=>{
-                if(data.startsWith('<!DOCTYPE html>'))return;
-                var samples=JSON.parse(data).currentData.problem.samples;
-                var code=``;
-                samples.forEach((sample,index)=>{
-                    code+=`===== Sample #${index+1} Input =====\n${sample[0]}\n===== Sample #${index+1} Output =====\n${sample[1]}\n`;
+var convertSample = (sample) => {
+    sample = sample.split('\n');
+    sample.forEach((line, index) => {
+        line = line.split('\r').join('');
+        while (line.length && "\r \t".includes(line.slice(-1)))
+            line = line.slice(0, -1);
+        sample[index] = line;
+    });
+    while (sample.length && sample.slice(-1)[0].length == 0)
+        sample = sample.slice(0, -1);
+    return sample.join('\n');
+};
+
+var getProblem = (id) => {
+    if (id == problemIdList.length) return;
+    if (Math.floor(id / 100) + 1 != currentJsonId)
+        current = getCurrentData(type,
+            currentJsonId = Math.floor(id / 100) + 1);
+    var PID = problemIdList[id];
+    if (current[PID]) getProblem(id + 1);
+    else {
+        console.log(`Getting Problem ${PID} (${id + 1}/${problemIdList.length})`);
+        https.get(`https://www.luogu.com.cn/problem/${PID}?_contentOnly=1`, (res) => {
+            let data = '';
+            res.on('data', chunk => data += chunk);
+            res.on('end', () => {
+                if (data.startsWith('<!DOCTYPE html>')) {
+                    console.log(`Failed: Getting Problem ${PID}`);
+                    return;
+                }
+                var samples = JSON.parse(data).currentData.problem.samples;
+                var result = new Array();
+                samples.forEach(sample => {
+                    result.push(convertSample(sample[0]));
+                    result.push(convertSample(sample[1]));
                 });
-                writeFileSync(`data/${type}/${PID}`,code);
+                current[PID] = { s: result };
+                writeFileSync(`data/${type}/${currentJsonId}.json`, JSON.stringify(current));
             });
         });
-        setTimeout(()=>getProblem(id+1),Number(process.argv[3]));
+        setTimeout(() => getProblem(id + 1), Number(process.argv[3]));
     }
 }
 
